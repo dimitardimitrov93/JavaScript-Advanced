41ebc79c76535016e6adfab4801294a9a1e75436
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-09-22 19:58:22 +0300

    commit

---
+++ Lecture № 2 - Arrays/Exercise/07. Magic Matrices.js
@@ -0,0 +1,37 @@
+function solve(arrInput) {
+
+    let matrix = arrInput.slice();
+    let columnsArr = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        let tempArr = [];
+        for (let j = 0; j < matrix.length; j++) {
+            tempArr.push(matrix[j][i])
+        }
+
+        columnsArr.push(tempArr);
+    }
+
+    let flattenedColArr = columnsArr.map(x => x.reduce((acc, currentValue) => acc + currentValue, 0));
+    isWithMagicalCols = flattenedColArr.every(x => x === flattenedColArr[0]);
+    
+    let flattenedRowArr = matrix.map(x => x.reduce((acc, currentValue) => acc + currentValue, 0));
+    let isWithMagicalRows = flattenedRowArr.every(x => x === flattenedRowArr[0]);
+
+    if (isWithMagicalRows && isWithMagicalCols) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+// solve([
+//     [1, 1],
+// ]);
+
+solve([
+    [4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]
+]);
