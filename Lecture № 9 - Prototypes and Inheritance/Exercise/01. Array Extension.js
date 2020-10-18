dee498abe3c36f88ab93c3ed0b11ef6dde278e81
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-10-18 18:40:22 +0300

    commit

---
+++ Lecture № 9 - Prototypes and Inheritance/Exercise/01. Array Extension.js
@@ -0,0 +1,22 @@
+(function solve() {
+
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    };
+
+    Array.prototype.skip = function (n) {
+        return this.slice(n);
+    };
+
+    Array.prototype.take = function (n) {
+        return this.slice(0, n + 1);
+    };
+
+    Array.prototype.sum = function () {
+        return this.reduce((acc, x) => acc + x, 0);
+    };
+
+    Array.prototype.average = function () {
+        return this.reduce((acc, x) => acc + x, 0) / this.length;
+    };
+})();
